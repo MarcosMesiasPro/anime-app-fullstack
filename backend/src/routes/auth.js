0bf914bd60ae3b0dfffdfa411ca712b5d4ec8806
Marcos Mesias <marcosmesiaspro@gmail.com>
2026-02-28 17:07:22 -0500

    debug #2: temporarily disable rate limiting

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -7,6 +7,8 @@ const { authLimiter } = require('../middleware/rateLimiter'); // ← NUEVO
 // ✅ Apply auth limiter to login/register
 // router.post('/register', authLimiter, register);
 // router.post('/login', authLimiter, login);
+router.post('/register', register);
+router.post('/login', login);
 
 // Protected route
 router.get('/me', protect, getMe);
